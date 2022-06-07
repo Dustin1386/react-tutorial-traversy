8a408b9197c703aa10de23b67af5bdd2c8292dea
dustin <dustinchesnutt@yahoo.com>
2022-06-06 23:38:04 -0700

    created dropdown and dd validation

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -3,18 +3,21 @@ import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import Button from "./shared/Button";
 import * as yup from "yup"
-
+import { States } from "../Data.js";
+const stateArray = States.map(e => e.abbreviation)
 const schema = yup.object().shape({
     fname: yup.string().required(),
     lname: yup.string().required(),
     email: yup.string().email().required(),
-    password: yup.string().min(4).max(30).required()
+    password: yup.string().min(4).max(30).required(),
+    states: yup.string().oneOf(States.map((e) => e.abbreviation)).required()
 
 
 })
 
 
 function Registration() {
+  console.log()
     const {register, handleSubmit, formState:{errors, isValid, isDirty}, reset,} = useForm({
         resolver: yupResolver(schema),
         mode: 'onChange',
@@ -23,6 +26,7 @@ function Registration() {
           lname: '',
           email: '',
           password: '',
+          states:'AL',
         }
     });
 
@@ -31,7 +35,7 @@ function Registration() {
       console.log(data)
 
     }
-    console.log(isValid, 'isDirty')
+    console.log(isValid)
 
   return (
     <>
@@ -43,7 +47,13 @@ function Registration() {
         <input type="text" name="email" placeholder="Email" {...register('email', { required: true })}/>
         {errors?.email && <p>{errors.email.message}</p>}        
         <input type="text" name="password"  placeholder="Password" {...register('password', { required: true })}/>
-        {errors?.password && <p>{errors.password.message}</p>}        
+        {errors?.password && <p>{errors.password.message}</p>} 
+        <select name="states" placeholder="States" {...register('states', { required: true })}>
+        <option value="" selected disabled hidden>{stateArray[0]}</option>
+        {
+          States.map(el => <option value={el.abbreviation} key={el.abbreviation}> {el.abbreviation} </option>)
+        }
+      </select>       
 
 
 
