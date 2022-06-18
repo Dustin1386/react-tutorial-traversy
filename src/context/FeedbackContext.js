d146d35842bb3ecdf24d78cb3f85ba048f9d078c
dustin <dustinchesnutt@yahoo.com>
2022-06-17 22:57:18 -0700

    set todo for v2

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -6,17 +6,45 @@ const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({children}) =>{
     const [feedback, setFeedback] = useState([
-        {
-            id:1,
-            text: 'this is from context',
-            rating: 10,
-        }
-    ])
+      {
+        id: 1,
+        text: "this is from context",
+        rating: 4,
+      },
+      {
+        id: 2,
+        text: "item 2",
+        rating: 6,
+      },
+      {
+        id: 3,
+        text: "item 3",
+        rating: 7,
+      },
+      {
+        id: 4,
+        text: "item 4",
+        rating: 10,
+      },
+    ]);
+    //TODO: V2
+    // const [feedbackEdit, setEditItem] = useState({
+    //   item: {},
+    //   edit: false
+    // })
     const deleteFeedback = (id) => {
         if (window.confirm("you really want to do this homie")) {
           setFeedback(feedback.filter((item) => item.id !== id));
         }
       };
+      // TODO: V2
+      // const editFeedback = (item) =>{
+      //   console.log(item, 'item')
+      //   setEditItem({
+      //     item,
+      //     edit :true
+      //   })
+      // }
       const addFeedback = (newFeedback) => {
         newFeedback.id = uuidv4();
         setFeedback([newFeedback, ...feedback]);
@@ -27,6 +55,8 @@ export const FeedbackProvider = ({children}) =>{
         feedback,
         deleteFeedback,
         addFeedback,
+        editFeedback,
+        feedbackEdit
 
     }}>
         {children}
