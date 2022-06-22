0c2ff5c2c8c2c9536af37340bf80802ffd023ddc
dustin <dustinchesnutt@yahoo.com>
2022-06-21 22:03:10 -0700

    created post

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,6 +1,4 @@
 import { createContext, useState, useEffect } from "react";
-import { v4 as uuidv4 } from "uuid";
-
 
 const FeedbackContext = createContext()
 
@@ -12,7 +10,7 @@ export const FeedbackProvider = ({children}) =>{
     }, [])
 
     const fetchFeedback = async () => {
-      const response = await fetch('http://localhost:5000/feedback?_sort=id&_order=desc')
+      const response = await fetch('/feedback?_sort=id&_order=desc')
       const data = await response.json()
       setFeedback(data)
     }
@@ -34,9 +32,16 @@ export const FeedbackProvider = ({children}) =>{
       //     edit :true
       //   })
       // }
-      const addFeedback = (newFeedback) => {
-        newFeedback.id = uuidv4();
-        setFeedback([newFeedback, ...feedback]);
+      const addFeedback = async (newFeedback) => {
+        const response = await fetch('/feedback', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body:JSON.stringify(newFeedback)
+        })
+        const data = await response.json()
+        setFeedback([data, ...feedback]);
       };
 
     return <FeedbackContext.Provider 
